c3d2a47761e81f56cd5e57df07689677c8d52190
Eric Bui <EricBui@ExSmart.co>
2019-04-22 00:53:41 +0800

    update entity class

--- src/Repository/CustomizedServiceEntityRepository.php
+++ src/Repository/CustomizedServiceEntityRepository.php
@@ -8,13 +8,32 @@
 
 namespace App\Repository;
 
+use Symfony\Bridge\Doctrine\RegistryInterface;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
+
+/**
+ * Optional EntityRepository base class with a simplified constructor (for autowiring).
+ *
+ * To use in your class, inject the "registry" service and call
+ * the parent constructor. For example:
+ *
+ * class YourEntityRepository extends ServiceEntityRepository
+ * {
+ *     public function __construct(RegistryInterface $registry)
+ *     {
+ *         parent::__construct($registry, YourEntity::class);
+ *     }
+ * }
+ */
 class CustomizedServiceEntityRepository extends ServiceEntityRepository
 {
     protected $entityRepo;
     public function __construct(RegistryInterface $registry, $entityClass)
     {
+        /**
+         * @param string $entityClass The class name of the entity this repository manages
+         */
         parent::__construct($registry, $entityClass);
         $this->entityRepo = $this->getDoctrine()->getManager()->getRepository($entityClass);
 
